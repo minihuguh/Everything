b01d2f0be358be5d95658ef7ff93c3f8c5efb248
minihuguh <zproprueba2022@protonmail.com>
2026-08-14 17:15:49 -0400

    Updated lib.rs to add new function "Minimize to tray"

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,27 @@
+// use tauri::Manager;
 mod audio;
 
-use std::sync::Mutex;
 use audio::player::AudioPlayer;
-use audio::symphonia_source::SymphoniaSource;
+use std::sync::Mutex;
+// use audio::symphonia_source::SymphoniaSource;
 use tauri::State;
+use tauri::{
+    include_image,
+    menu::{Menu, MenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent}, Manager,
+};
+// use tauri::{
+//     include_image,
+//     menu::{Menu, MenuItem},
+//     tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+//     Listener, Manager, WindowEvent,
+// };
+// #[cfg(target_os = "windows")]
+// use tauri::utils::config::WindowConfig;
 
 pub struct AppState {
     player: Mutex<AudioPlayer>,
 }
-
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -60,11 +73,75 @@ fn is_playing(state: State<AppState>) -> bool {
     state.player.lock().unwrap().is_playing()
 }
 
+//noinspection D
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let player = AudioPlayer::new().expect("No se pudo crear el reproductor");
-
+    // std::env::set_var("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS",
+    //                   "--single-process \
+    //      --disable-gpu \
+    //      --disable-gpu-compositing \
+    //      --disable-features=NetworkServiceInProcess,Translate,SharingHub,Extensions, msWebOOUI,msAutofillEdgeCoupled \
+    //      --disable-crash-reporter \
+    //      --disable-translate \
+    //      --disable-features=InterestFeedContentSuggestions \
+    //      --disable-features=CalculateNativeWinOcclusion \
+    //      --disable-default-apps \
+    //      --disable-sync \
+    //      --js-flags='--max-semi-space-size=1 --max-old-space-size=32'"
+    // );
     tauri::Builder::default()
+        .setup(|app| {
+            // 1. Crear un menú sencillo para el Tray (opcional, p. ej. botón Salir)
+            let quit_i = MenuItem::with_id(app, "quit", "Salir", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_i])?;
+
+            // 2. Cargar el icono del Tray (asegúrate de que exista en src/icons/)
+            let icon = include_image!("./icons/icon.ico");
+
+            // 3. Construir el Tray Icon
+            let _tray = TrayIconBuilder::new()
+                .icon(icon)
+                .menu(&menu)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } = event
+                    {
+                        // Al hacer clic izquierdo en el tray, mostramos la ventana principal
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.unminimize();
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                })
+                .build(app)?;
+
+            // 4. Intercepta el evento de la ventana para ocultarla cuando se minimice
+            if let Some(window) = app.get_webview_window("main") {
+                let window_clone = window.clone();
+                window.on_window_event(move |event| {
+                    if let tauri::WindowEvent::Resized(_) = event {
+                        // Verificamos de forma segura si la ventana se encuentra minimizada
+                        if let Ok(true) = window_clone.is_minimized() {
+                            let _ = window_clone.hide();
+                        }
+                    }
+                });
+            }
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_fs::init())
         .manage(AppState {
@@ -84,4 +161,3 @@ pub fn run() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
-
