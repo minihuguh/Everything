1e07b7b637ec42f4f780b7743ae134c1e9a6dc31
minihuguh <zproprueba2022@protonmail.com>
2026-08-29 13:10:27 -0400

    feat(layout): new signal for queue

--- src/components/layout.rs
+++ src/components/layout.rs
@@ -1,7 +1,7 @@
 use dioxus::prelude::*;
 use crate::components::{sidebar::Sidebar, player_bar::PlayerBar, content_header::ContentHeader};
 use crate::state::{use_player_state, provide_player_state};
-use crate::views::{home::Home, search::Search, lyrics::Lyrics, playlist::Playlist};
+use crate::views::{home::Home, search::Search, lyrics::Lyrics, playlist::PlaylistView, stadistics::Stadistics, queue::QueuePanel};
 
 #[component]
 pub fn Layout() -> Element {
@@ -10,6 +10,7 @@ pub fn Layout() -> Element {
 
     let player = use_player_state();
     let current_view = use_memo(move || player().current_view);
+    let mut show_queue = use_signal(|| false);
 
     rsx! {
         div { class: "app-layout",
@@ -24,14 +25,20 @@ pub fn Layout() -> Element {
                             "home" => rsx! { Home {} },
                             "search" => rsx! { Search {} },
                             "lyrics" => rsx! { Lyrics {} },
-                            "playlist" => rsx! { Playlist {} },
+                            "playlist" => rsx! { PlaylistView {} },
+                            "stadistics" => rsx! { Stadistics {} },
                             _ => rsx! { Home {} },
                         }
                     }
                 }
             }
 
-            PlayerBar {}
+            PlayerBar { show_queue}
+            QueuePanel { show: show_queue }
+            div {
+                class: if show_queue() { "queue-backdrop open" } else { "queue-backdrop" },
+                onclick: move |_| show_queue.set(false),
+            }
         }
     }
 }
