f7c47a1053b657e3fc8ba1f4677994eb972acd76
minihuguh <zproprueba2022@protonmail.com>
2026-09-05 10:28:21 -0400

    refactor(lib): new command file and removed deprecated functions and comments

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,142 @@
+use std::sync::mpsc;
+use std::time::Duration;
+use log::{error, info};
+use serde_json::Value;
+use tauri::State;
+use tauri_plugin_dialog::DialogExt;
+use tauri_plugin_fs::FilePath;
+use crate::AppState;
+use crate::state::TrackMetadata;
+
+#[tauri::command]
+pub fn minimize_window(window: tauri::WebviewWindow) {
+    let _ = window.minimize();
+}
+
+#[tauri::command]
+pub fn toggle_maximize(window: tauri::WebviewWindow) {
+    if window.is_maximized().unwrap_or(false) {
+        let _ = window.unmaximize();
+    } else {
+        let _ = window.maximize();
+    }
+}
+
+#[tauri::command]
+pub fn close_window(window: tauri::WebviewWindow) {
+    let _ = window.close();
+}
+
+#[tauri::command]
+pub fn play_file(state: State<AppState>, path: String) -> Result<Value, String> {
+    info!("Operación exitosa. Datos a enviar: {:?}", path);
+
+    let a = state.player.lock().unwrap().play_file(&path);
+    match a {
+        Ok(valor) => {
+            // info!("Operación exitosa. Datos recibidos: {:?}", valor);
+            Ok(valor)
+        }
+        Err(mensaje_error) => {
+            error!("Falló la operación. Motivo: {}", mensaje_error);
+            Err(mensaje_error)
+        }
+    }
+}
+
+#[tauri::command]
+pub fn pause(state: State<AppState>) {
+    state.player.lock().unwrap().pause();
+}
+
+#[tauri::command]
+pub fn resume(state: State<AppState>) {
+    state.player.lock().unwrap().resume();
+}
+
+#[tauri::command]
+pub fn stop(state: State<AppState>) {
+    state.player.lock().unwrap().stop();
+}
+
+#[tauri::command]
+pub fn set_volume(state: State<AppState>, volume: f32) {
+    state.player.lock().unwrap().set_volume(volume);
+}
+
+#[tauri::command]
+pub fn get_time(state: State<AppState>) -> u64 {
+    let valor = state.player.lock().unwrap().get_time();
+    info!("Operación exitosa. Datos recibidos: {:?}", valor.to_string());
+    valor
+}
+
+#[tauri::command]
+pub fn set_time(state: State<AppState>, time_in_seconds: u64) -> Result<(), String> {
+    let duration = Duration::from_secs(time_in_seconds);
+    // let temp_duration = Duration::from_secs(30);
+    let temp = state.player.lock().unwrap().set_time(duration);
+    info!("Datos recibidos desde set_time: {:?}", temp);
+
+    Ok(())
+}
+
+#[tauri::command]
+pub fn open_playlist<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> Option<FilePath> {
+    let (tx, rx) = mpsc::channel();
+
+    app.dialog()
+        .file()
+        .add_filter("Text Files", &["xspf", "xml"])
+        .pick_file(move |file_path| {
+            let _ = tx.send(file_path);
+        });
+
+    // let response = rx.recv().map_err(|e| e.to_string())?;
+
+    rx.recv().unwrap_or(None)
+}
+
+#[tauri::command]
+pub fn select_document<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> Option<FilePath> {
+    let (tx, rx) = mpsc::channel();
+
+    app.dialog()
+        .file()
+        .add_filter("Text Files", &["opus", "md"])
+        .pick_file(move |file_path| {
+            let _ = tx.send(file_path);
+        });
+
+    // let response = rx.recv().map_err(|e| e.to_string())?;
+
+    rx.recv().unwrap_or(None)
+}
+
+#[tauri::command]
+pub fn get_metadata(state: State<AppState>, path: String) -> TrackMetadata {
+    info!("Operación exitosa. Datos a enviar: {:?}", path);
+
+    let a = state.player.lock().unwrap().extract_metadata(&path);
+    match a {
+        Ok(valor) => {
+            // info!("Operación exitosa. Datos recibidos: {:?}", valor);
+            valor
+        }
+        Err(mensaje_error) => {
+            error!("Falló la operación. Motivo: {}", mensaje_error);
+            TrackMetadata {
+                title: "".to_string(),
+                artist: "".to_string(),
+                duration: 0.0,
+                path,
+                image: "".to_string(),
+            }
+        }
+    }
+}
+
+#[tauri::command]
+pub fn is_playing(state: State<AppState>) -> bool {
+    state.player.lock().unwrap().is_playing()
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,228 +1,28 @@
-// use tauri::Manager;
 mod audio;
 mod state;
+mod commands;
 
 use tauri_plugin_log::{Target, TargetKind};
 use tauri_plugin_dialog::{DialogExt, FilePath};
 use audio::player::AudioPlayer;
-use std::sync::{mpsc, Mutex};
-use std::time::Duration;
-use log::{error, info};
-use serde_json::Value;
+use std::sync::{Mutex};
+use log::{debug, error, info, warn};
 // use audio::symphonia_source::SymphoniaSource;
-use tauri::State;
-use tauri::{
-    include_image,
-    menu::{Menu, MenuItem},
-    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent}, Manager,
-};
-use crate::state::TrackMetadata;
-// use tauri::{
-//     include_image,
-//     menu::{Menu, MenuItem},
-//     tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
-//     Listener, Manager, WindowEvent,
-// };
-// #[cfg(target_os = "windows")]
-// use tauri::utils::config::WindowConfig;
+
+use tauri::Manager;
+use crate::commands::*;
 
 pub struct AppState {
     player: Mutex<AudioPlayer>,
 }
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn minimize_window(window: tauri::WebviewWindow) {
-    let _ = window.minimize();
-}
-
-#[tauri::command]
-fn toggle_maximize(window: tauri::WebviewWindow) {
-    if window.is_maximized().unwrap_or(false) {
-        let _ = window.unmaximize();
-    } else {
-        let _ = window.maximize();
-    }
-}
-
-#[tauri::command]
-fn close_window(window: tauri::WebviewWindow) {
-    let _ = window.close();
-}
-
-#[tauri::command]
-fn play_file(state: State<AppState>, path: String) -> Result<Value, String> {
-    info!("Operación exitosa. Datos a enviar: {:?}", path);
-
-    let a = state.player.lock().unwrap().play_file(&path);
-    match a {
-        Ok(valor) => {
-            // info!("Operación exitosa. Datos recibidos: {:?}", valor);
-            // Al no poner punto y coma aquí, este Ok es lo que la función retorna
-            Ok(valor)
-        }
-        Err(mensaje_error) => {
-            error!("Falló la operación. Motivo: {}", mensaje_error);
-            // Al no poner punto y coma aquí, este Err es lo que la función retorna
-            Err(mensaje_error)
-        }
-    }
-}
-
-#[tauri::command]
-fn pause(state: State<AppState>) {
-    state.player.lock().unwrap().pause();
-}
-
-#[tauri::command]
-fn resume(state: State<AppState>) {
-    state.player.lock().unwrap().resume();
-}
-
-#[tauri::command]
-fn stop(state: State<AppState>) {
-    state.player.lock().unwrap().stop();
-}
-
-#[tauri::command]
-fn set_volume(state: State<AppState>, volume: f32) {
-    state.player.lock().unwrap().set_volume(volume);
-}
-
-#[tauri::command]
-fn get_time(state: State<AppState>) -> u64 {
-    let valor = state.player.lock().unwrap().get_time();
-    info!("Operación exitosa. Datos recibidos: {:?}", valor.to_string());
-    valor
-}
-
-#[tauri::command]
-fn set_time(state: State<AppState>, time_in_seconds: u64) -> Result<(), String> {
-    // 2. Convertimos el u64 a Duration
-    let duration = Duration::from_secs(time_in_seconds);
-    // let temp_duration = Duration::from_secs(30);
-    // 3. Agregamos el punto y coma final para separar las expresiones
-    let temp = state.player.lock().unwrap().set_time(duration);
-    info!("Datos recibidos desde set_time: {:?}", temp);
-
-    // 4. Devolvemos la señal de éxito
-    Ok(())
-}
-
-#[tauri::command]
-fn select_document<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> Option<FilePath> {
-    let (tx, rx) = mpsc::channel();
-
-    app.dialog()
-        .file()
-        .add_filter("Text Files", &["opus", "md"])
-        .pick_file(move |file_path| {
-            let _ = tx.send(file_path);
-        });
-
-    // let response = rx.recv().map_err(|e| e.to_string())?;
-
-    rx.recv().unwrap_or(None)
-}
-
-#[tauri::command]
-fn get_metadata(state: State<AppState>, path: String) -> TrackMetadata {
-    info!("Operación exitosa. Datos a enviar: {:?}", path);
-
-    let a = state.player.lock().unwrap().extract_metadata(&path);
-    match a {
-        Ok(valor) => {
-            // info!("Operación exitosa. Datos recibidos: {:?}", valor);
-            // Al no poner punto y coma aquí, este Ok es lo que la función retorna
-            valor
-        }
-        Err(mensaje_error) => {
-            error!("Falló la operación. Motivo: {}", mensaje_error);
-            TrackMetadata {
-                title: "".to_string(),
-                artist: "".to_string(),
-                duration: 0.0,
-                path,
-                image: "".to_string(),
-            }
-        }
-    }
-}
-
-#[tauri::command]
-fn is_playing(state: State<AppState>) -> bool {
-    state.player.lock().unwrap().is_playing()
-}
 
 //noinspection D
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let player = AudioPlayer::new().expect("No se pudo crear el reproductor");
-    // std::env::set_var("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS",
-    //                   "--single-process \
-    //      --disable-gpu \
-    //      --disable-gpu-compositing \
-    //      --disable-features=NetworkServiceInProcess,Translate,SharingHub,Extensions, msWebOOUI,msAutofillEdgeCoupled \
-    //      --disable-crash-reporter \
-    //      --disable-translate \
-    //      --disable-features=InterestFeedContentSuggestions \
-    //      --disable-features=CalculateNativeWinOcclusion \
-    //      --disable-default-apps \
-    //      --disable-sync \
-    //      --js-flags='--max-semi-space-size=1 --max-old-space-size=32'"
-    // );
     tauri::Builder::default()
-        .setup(|app| {
-            // 1. Crear un menú sencillo para el Tray (opcional, p. ej. botón Salir)
-            let quit_i = MenuItem::with_id(app, "quit", "Salir", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&quit_i])?;
-
-            // 2. Cargar el icono del Tray (asegúrate de que exista en src/icons/)
-            let icon = include_image!("./icons/icon.ico");
-
-            // 3. Construir el Tray Icon
-            let _tray = TrayIconBuilder::new()
-                .icon(icon)
-                .menu(&menu)
-                .on_menu_event(|app, event| match event.id.as_ref() {
-                    "quit" => {
-                        app.exit(0);
-                    }
-                    _ => {}
-                })
-                .on_tray_icon_event(|tray, event| {
-                    if let TrayIconEvent::Click {
-                        button: MouseButton::Left,
-                        button_state: MouseButtonState::Up,
-                        ..
-                    } = event
-                    {
-                        // Al hacer clic izquierdo en el tray, mostramos la ventana principal
-                        let app = tray.app_handle();
-                        if let Some(window) = app.get_webview_window("main") {
-                            let _ = window.unminimize();
-                            let _ = window.show();
-                            let _ = window.set_focus();
-                        }
-                    }
-                })
-                .build(app)?;
-
-            // 4. Intercepta el evento de la ventana para ocultarla cuando se minimice
-            if let Some(window) = app.get_webview_window("main") {
-                let window_clone = window.clone();
-                window.on_window_event(move |event| {
-                    if let tauri::WindowEvent::Resized(_) = event {
-                        // Verificamos de forma segura si la ventana se encuentra minimizada
-                        if let Ok(true) = window_clone.is_minimized() {
-                            let _ = window_clone.hide();
-                        }
-                    }
-                });
-            }
-
-            Ok(())
-        })
         .setup(|app| {
             #[cfg(debug_assertions)]
             {
@@ -232,13 +32,11 @@ pub fn run() {
             Ok(())
         })
         .plugin(tauri_plugin_fs::init())
-        // .plugin(tauri_plugin_prevent_default::init())
         .plugin(
             tauri_plugin_log::Builder::new()
-                // Define dónde quieres ver los logs (en la Terminal y/o en un archivo físico)
                 .targets([
-                    Target::new(TargetKind::Stdout), // Muestra logs en la consola/terminal
-                    Target::new(TargetKind::LogDir { file_name: Some("logs".to_string()) }), // Opcional: Guarda logs en archivos .log
+                    Target::new(TargetKind::Stdout),
+                    Target::new(TargetKind::LogDir { file_name: Some("logs".to_string()) }),
                 ])
                 .build(),
         )
@@ -259,7 +57,8 @@ pub fn run() {
             get_time,
             set_time,
             select_document,
-            get_metadata
+            get_metadata,
+            open_playlist
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
